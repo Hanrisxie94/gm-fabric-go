6a217a326ca036305a35391e2b9e6971aafb9e84
rob.fielding <rob.fielding@deciphernow.com>
2018-01-24 11:32:37 -0500

    enable race detect if cgo enabled, warn otherwise

--- cmd/fabric/build_fabric.sh
+++ cmd/fabric/build_fabric.sh
@@ -18,9 +18,16 @@ set -euxo pipefail
 
 SRVGEN_PATH=$GOPATH/src/github.com/deciphernow/gm-fabric-go/cmd/fabric
 
+# If cgo is explicitly disabled, then we can't use the race detection flag
+GOFLAGS=-race
+if [ "x$CGO_ENABLED" == "x0" ]
+then
+  echo Go race condition detection is disabled in this build, because you disabled CGO
+  GOFLAGS=
+fi
+
 pushd $SRVGEN_PATH
 GITHASH=`(git rev-parse --verify --short HEAD)`
-go install \
-    -race \
+go install $GOFLAGS \
     -ldflags "-X github.com/deciphernow/gm-fabric-go/version.gitHash=${GITHASH}"
 popd
