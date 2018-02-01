4a814d62ef0e778fe8581160bf104c7f7add4f22
dougfort <doug.fort@gmail.com>
2018-02-01 10:58:37 -0500

    fetch from .../default/templates instead of .../default

--- cmd/fabric/templates/fetch.go
+++ cmd/fabric/templates/fetch.go
@@ -7,13 +7,13 @@ import (
 	"path"
 	"path/filepath"
 	t "text/template"
-	
+
 	"github.com/hashicorp/go-getter"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 )
 
-// The callback function for invocations of the templates.Fetch method. The set of templates yielded to this method 
+// The callback function for invocations of the templates.Fetch method. The set of templates yielded to this method
 // represent the name (i.e., relative path as a template) and content of a fetched file. This enables file names and
 // paths to be treated as templates in addition to thier contents.
 type Callback func(name *t.Template, content *t.Template, mode os.FileMode) error
@@ -27,6 +27,7 @@ func Fetch(source string, callback Callback, logger zerolog.Logger) error {
 	var err error
 	var tmp string
 	var destination string
+	var templates string
 
 	if tmp, err = ioutil.TempDir("", "fabric-"); err != nil {
 		return errors.Wrapf(err, "error creating temporary directory for templates from %s", source)
@@ -35,6 +36,8 @@ func Fetch(source string, callback Callback, logger zerolog.Logger) error {
 	defer os.RemoveAll(tmp)
 
 	destination = path.Join(tmp, "repository")
+	// TODO: read and unmarshall destination/fabric.json
+	templates = path.Join(destination, "templates")
 
 	var walker = func(absolute string, info os.FileInfo, err error) error {
 
@@ -44,39 +47,39 @@ func Fetch(source string, callback Callback, logger zerolog.Logger) error {
 		var content *t.Template
 
 		if err != nil {
-			return errors.Wrapf(err, "error walking the template directory %s", destination) 
+			return errors.Wrapf(err, "error walking the template directory %s", templates)
 		}
 
-		if relative, err = filepath.Rel(destination, absolute); err != nil {
-			return errors.Wrapf(err, "error determining the relative path from %s to %s", destination, absolute) 
+		if relative, err = filepath.Rel(templates, absolute); err != nil {
+			return errors.Wrapf(err, "error determining the relative path from %s to %s", templates, absolute)
 		}
 
 		if info.Mode().IsDir() {
-            logger.Debug().Msg(fmt.Sprintf("Ignoring template directory: %s", relative))
+			logger.Debug().Msg(fmt.Sprintf("Ignoring template directory: %s", relative))
 			return nil
 		}
 
 		if name, err = t.New(relative).Parse(relative); err != nil {
-			return errors.Wrapf(err, "error parsing the path of %s as a template", relative) 
+			return errors.Wrapf(err, "error parsing the path of %s as a template", relative)
 		}
 
 		if bytes, err = ioutil.ReadFile(absolute); err != nil {
-			return errors.Wrapf(err, "error reading template from %s", absolute) 
+			return errors.Wrapf(err, "error reading template from %s", absolute)
 		}
 
 		if content, err = t.New(relative).Parse(string(bytes)); err != nil {
-			return errors.Wrapf(err, "error parsing the content of %s as a template", relative) 
+			return errors.Wrapf(err, "error parsing the content of %s as a template", relative)
 		}
 
 		return callback(name, content, info.Mode().Perm())
 	}
 
 	if err = getter.Get(destination, source); err != nil {
-		return errors.Wrapf(err, "error copying templates from %s to %s", source, destination) 
+		return errors.Wrapf(err, "error copying templates from %s to %s", source, destination)
 	}
 
-	if err = filepath.Walk(destination, walker); err != nil {
-		return errors.Wrapf(err, "error invoking callback for fetcher") 
+	if err = filepath.Walk(templates, walker); err != nil {
+		return errors.Wrapf(err, "error invoking callback for fetcher: %s", templates)
 	}
 
 	return nil
