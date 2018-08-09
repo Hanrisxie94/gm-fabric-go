cb193937cbf3aadbbb12f1b7bdb0a5499cce1caa
dougfort <doug.fort@gmail.com>
2018-08-09 10:23:24 -0400

    start v0.1.11-pre (#319)

--- version/current.go
+++ version/current.go
@@ -18,7 +18,7 @@ import "fmt"
 
 // semver is the semantic version
 // see https://semver.org/
-const semver = "0.1.10"
+const semver = "0.1.11-pre"
 
 // gitHash should be filled in at compile time with
 // GITHASH=`(git rev-parse --verify --short HEAD)`
